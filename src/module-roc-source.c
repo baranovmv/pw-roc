729d67c3001e3e97265713b4213ebfdd27bb65be
Misha Baranov <baranov.mv@gmail.com>
2026-07-24 13:19:37 +0200

    Hot fix source: add media class onto source

--- src/module-roc-source.c
+++ src/module-roc-source.c
@@ -484,6 +484,8 @@ int pipewire__module_init(struct pw_impl_module *module, const char *args)
 		pw_properties_set(playback_props, PW_KEY_NODE_VIRTUAL, "true");
 	if (pw_properties_get(playback_props, PW_KEY_NODE_NETWORK) == NULL)
 		pw_properties_set(playback_props, PW_KEY_NODE_NETWORK, "true");
+	if (pw_properties_get(playback_props, PW_KEY_MEDIA_CLASS) == NULL)
+		pw_properties_set(playback_props, PW_KEY_MEDIA_CLASS, "Audio/Source");
 
 	if ((str = pw_properties_get(props, SPA_KEY_AUDIO_POSITION)) != NULL) {
 		pw_properties_set(playback_props, SPA_KEY_AUDIO_POSITION, str);
